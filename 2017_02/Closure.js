a3adebae99efc500f28daac20f57cf891396d8db
Hyun <sunghyunpd@gmail.com>
2017-02-17 15:48:18 +0900

    주석 수정

--- 2017_02/Closure.js
+++ 2017_02/Closure.js
@@ -10,7 +10,7 @@ function add(a,b,callback){
 	var count = 0;
 	var history = function(){
 		count++;
-		return "history 호출 횟수 "+ count + ' : '+ a + '+' + b + '=' + result;
+		return "history 호출 횟수 "+ count + '\t add() 결과는 '+ a + '+' + b + '=' + result;
 	};
 	return history;
 }
@@ -20,15 +20,7 @@ var addHistory = add(10,10, function(result){
 });
 console.log(addHistory());
 
-var addHistory2 = add(20,20, function(result){
-	console.log(" 콜백 함수 파라미터 : 20 + 20 = " + result);
-});
-console.log(addHistory2());
-
-console.log("\n");
-
 // add()함수의 history()를 호출해서 add의 count값을 증가시킨다.
-// history()함수에서 사용하는 count변수 접근이 가능하다.
+// history()함수에서 add()함수에 있는 a,b,result,count변수에 접근이 가능하다.
 console.log(addHistory());
-console.log(addHistory2());
 
